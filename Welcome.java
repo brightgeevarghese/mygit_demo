bb6ca860aa068d4df7bf86202122c929b6ba13ae
Bright Gee Varghese <bvarghese@miu.edu>
2025-02-03 15:49:29 -0600

    Modified deals

--- Welcome.java
+++ Welcome.java
@@ -1,7 +1,7 @@
 public class Welcome {
     public static void main(String[] args) {
         System.out.println("Welcome to my app!");
-        System.out.println("Todays' deal...");
+        System.out.println("Todays' deal is extended to another 5 days");
         System.out.println("PayPal");
     }
 }
