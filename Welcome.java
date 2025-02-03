7c5935a11a61d136f517d7600ad3c574b4791d4f
Bright Gee Varghese <bvarghese@miu.edu>
2025-02-03 15:32:26 -0600

    Added PayPal feature

--- Welcome.java
+++ Welcome.java
@@ -2,5 +2,6 @@ public class Welcome {
     public static void main(String[] args) {
         System.out.println("Welcome to my app!");
         System.out.println("Todays' deal...");
+        System.out.println("PayPal");
     }
 }
