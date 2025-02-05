01d4757c60454ebabaa021c3a21efd9d1dc5ad7c
Bright Gee Varghese <bvarghese@miu.edu>
2025-02-05 17:00:19 -0600

    another commit

--- Welcome.java
+++ Welcome.java
@@ -2,7 +2,6 @@ public class Welcome {
     public static void main(String[] args) {
         System.out.println("Welcome to my app!");
         System.out.println("Todays' deal is extended to another 5 days");
-        System.out.println("PayPal");
         System.out.println("Added a new feature by another person");
         System.out.println("Apple Pay");
         System.out.println("Added a new feature .... again");
